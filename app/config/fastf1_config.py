ca90e27993f7f8a8a1383748c4a201444099ea62
akhil <akhil2448@gmail.com>
2026-07-20 20:44:15 -0500

    Make FastF1 cache path configurable

--- app/config/fastf1_config.py
+++ app/config/fastf1_config.py
@@ -1,4 +1,12 @@
+# import fastf1
+
+# def init_fastf1_cache(cache_dir="cache"):
+#     fastf1.Cache.enable_cache(cache_dir)
+
+
+import os
 import fastf1
 
-def init_fastf1_cache(cache_dir="cache"):
+def init_fastf1_cache():
+    cache_dir = os.getenv("FASTF1_CACHE_DIR", "cache")
     fastf1.Cache.enable_cache(cache_dir)
